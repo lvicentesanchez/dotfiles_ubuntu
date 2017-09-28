18a0f59c5a9cda4d5b751b707b99ddc3f1791b0f
Luis Vicente <langel.vicente@gmail.com>
2017-09-28 18:26:00 +0200

    Many tweaks too xmonad config

--- xmonad/xmobar.hs
+++ xmonad/xmobar.hs
@@ -1,8 +1,8 @@
 Config { font = "xft:Meslo LG M DZ for Powerline:size=10"
-       , bgColor = "black"
-       , fgColor = "grey"
+       , bgColor = "#002b36"
+       , fgColor = "#657b83"
        , position = Static { xpos = 0 , ypos = 0, width = 3520, height = 32 }
-       , commands = [ Run Cpu ["-L","3","-H","50","--normal","green","--high","red"] 10
+       , commands = [ Run Cpu ["-L","3","-H","50","--normal","#859900","--high","#dc322f"] 10
                     , Run Memory ["-t","Mem: <usedratio>%"] 10
                     , Run Swap [] 10
                     , Run Date "%F %H:%M" "date" 10
@@ -13,11 +13,11 @@ Config { font = "xft:Meslo LG M DZ for Powerline:size=10"
 	              --"-c", "charge_full",
 	              "-O", "AC",
 	              "-o", "Bat",
-	              "-h", "green",
-	              "-l", "red"
+	              "-h", "#859900",
+	              "-l", "#dc322f"
 	              ] 10
                     ]
        , sepChar = "%"
        , alignSep = "}{"
-       , template = "%StdinReader% }{ %cpu% | %memory% * %swap% | %battery% | <fc=#ee9a00>%date%</fc> |"
+       , template = "%StdinReader% }{ %cpu% <fc=#c0c0c0>|</fc> %memory% * %swap% <fc=#c0c0c0>|</fc> %battery% <fc=#c0c0c0>|</fc> %date% <fc=#c0c0c0>|</fc>"
        }
--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -18,34 +18,32 @@ main = do
             spawn "feh --bg-scale -z \"/home/luis/Pictures/Wallpapers/Cthulhu - 02.png\""            
             spawn "stalonetray -c ~/.config/stalonetrayrc"
             spawn "nm-applet"
-	    spawn "redshift-gtk"
+            spawn "redshift-gtk"
             spawn "urxvtd -f -o -q"
-        , manageHook = manageDocks <+> manageHook def
+        , normalBorderColor = myNormalBorderColor
+        , focusedBorderColor = myFocusedBorderColor
+	, manageHook = manageDocks <+> manageHook def
         , layoutHook = myLayout --avoidStruts  $  layoutHook def
         , logHook = dynamicLogWithPP $ xmobarPP
                   { ppOutput = hPutStrLn xmproc
-                  , ppTitle = xmobarColor "green" "" . shorten 50
+                  , ppTitle = xmobarColor "#657b83" "" . shorten 100   
+                  , ppCurrent = xmobarColor "#c0c0c0" "" . wrap "" ""
+                  , ppSep     = xmobarColor "#c0c0c0" "" " | "
+                  , ppUrgent  = xmobarColor "#ff69b4" ""
                   }
         , modMask = mod4Mask     
         } `additionalKeys`
         [ ((mod4Mask, xK_b)              , sendMessage ToggleStruts)
-        , ((mod4Mask, xK_d)              , spawn "rofi -show drun")
+        , ((mod4Mask, xK_d)              , spawn "rofi -bw 1 -show drun")
         , ((mod4Mask, xK_m)              , spawn "xrandr --output DP1 --auto --primary --left-of eDP1")
         , ((mod4Mask .|. shiftMask, xK_m), spawn "xrandr --output DP1 --off")
         , ((mod4Mask .|. shiftMask, xK_l), spawn "dm-tool lock")
         ]
 
-myLayout = avoidStruts $
-           emptyBSP
-	   ||| tiled
-           ||| Mirror tiled
-           ||| Full
-  where
-     -- default tiling algorithm partitions the screen into two panes
-     tiled   = Tall nmaster delta ratio
-     -- The default number of windows in the master pane
-     nmaster = 1
-     -- Default proportion of screen occupied by master pane
-     ratio   = 1/2
-     -- Percent of screen to increment by when resizing panes
-     delta   = 2/100
+myLayout = avoidStruts $ emptyBSP ||| Full
+
+myNormalBorderColor = "#002b36"
+
+myFocusedBorderColor = "#657b83"
+
+xmobarTitleColor = "green"
