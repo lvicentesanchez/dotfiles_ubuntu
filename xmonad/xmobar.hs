442902a06926cef63af09ef9d58bb0a2cc3f5f7c
Luis Vicente <langel.vicente@gmail.com>
2017-09-28 19:41:06 +0200

    Changes to xmobar config

--- xmonad/xmobar.hs
+++ xmonad/xmobar.hs
@@ -2,22 +2,46 @@ Config { font = "xft:Meslo LG M DZ for Powerline:size=10"
        , bgColor = "#002b36"
        , fgColor = "#657b83"
        , position = Static { xpos = 0 , ypos = 0, width = 3520, height = 32 }
-       , commands = [ Run Cpu ["-L","3","-H","50","--normal","#859900","--high","#dc322f"] 10
-                    , Run Memory ["-t","Mem: <usedratio>%"] 10
-                    , Run Swap [] 10
-                    , Run Date "%F %H:%M" "date" 10
+       , commands = [ -- cpu core temperature monitor
+                      Run CoreTemp       [ "--template" , "Temp: <core0>°C <core1>°C <core2>°C <core3>°C"
+                                         , "--Low"      , "40"        -- units: °C
+                                         , "--High"     , "80"        -- units: °C
+                                         , "--low"      , "#859900"
+                                         , "--normal"   , "#b58900"
+                                         , "--high"     , "#dc322f"
+                                         ] 50
+
+                      -- memory usage monitor
+                    , Run Memory         [ "--template" ,"Mem: <usedratio>%"
+                                         , "--Low"      , "20"        -- units: %
+                                         , "--High"     , "90"        -- units: %
+                                         , "--low"      , "#859900"
+                                         , "--normal"   , "#b58900"
+                                         , "--high"     , "#dc322f"
+                                         ] 10
+
+                    , Run Date           "%F %H:%M" "date" 10
+        
+	              -- battery monitor
+                    , Run Battery        [ "--template" , "Batt: <acstatus>"
+                                         , "--Low"      , "10"        -- units: %
+                                         , "--High"     , "80"        -- units: %
+                                         , "--low"      , "#dc322f"
+                                         , "--normal"   , "#b58900"
+                                         , "--high"     , "#859900"
+                                         , "--" -- battery specific options
+                                                   -- discharging status
+                                                   , "-o"	, "<left>% (<timeleft>)"
+                                                   -- AC "on" status
+                                                   , "-O"	, "<fc=#b58900>Charging</fc>"
+                                                   -- charged status
+                                                   , "-i"	, "<fc=#859900>Charged</fc>"
+
+                                         ] 30
+
                     , Run StdinReader
-                    , Run Battery [
-	              "-t", "<acstatus>: <left>% - <timeleft>",
-	              "--",
-	              --"-c", "charge_full",
-	              "-O", "AC",
-	              "-o", "Bat",
-	              "-h", "#859900",
-	              "-l", "#dc322f"
-	              ] 10
                     ]
        , sepChar = "%"
        , alignSep = "}{"
-       , template = "%StdinReader% }{ %cpu% <fc=#c0c0c0>|</fc> %memory% * %swap% <fc=#c0c0c0>|</fc> %battery% <fc=#c0c0c0>|</fc> %date% <fc=#c0c0c0>|</fc>"
+       , template = "%StdinReader% }{ <fc=#c0c0c0>|</fc> %coretemp% <fc=#c0c0c0>|</fc> %memory% <fc=#c0c0c0>|</fc> %battery% <fc=#c0c0c0>|</fc> %date% <fc=#c0c0c0>|</fc>"
        }
